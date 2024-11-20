4ed0288c2a3c01626c2c6b2807335e48d0e5a8d0
omid-faghihi <fhomid3@gmail.com>
2024-11-21 00:16:11 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,39 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 // import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 // import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    }),
-  ],
-});
-
-// import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // export default defineConfig({
-//   plugins: [
-//     react(),
-//     nodePolyfills({
-//       // Whether to polyfill `node:` protocol imports.
-//       protocolImports: true,
-//     })
-//   ]
-// })
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": "/src",
+//       buffer: "buffer",
+//     },
+//   },
+//   define: {
+//     global: "window",
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       plugins: [
+//         NodeGlobalsPolyfillPlugin({
+//           buffer: true,
+//         }),
+//         NodeModulesPolyfillPlugin(),
+//       ],
+//     },
+//   },
+// });
 
-// resolve: {
-//   alias: {
-//     "@": "/src", // Alias for your source directory
-//     buffer: "buffer", // Use the `buffer` package as a polyfill
-//   },
-// },
-// define: {
-//   global: "window", // Provide the global `window` object as a polyfill
-// },
-// optimizeDeps: {
-//   esbuildOptions: {
-//     // Polyfill Node.js globals like `Buffer` and `process`
-//     plugins: [
-//       NodeGlobalsPolyfillPlugin({
-//         buffer: true,
-//       }),
-//       NodeModulesPolyfillPlugin(),
-//     ],
-//   },
-// },
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
