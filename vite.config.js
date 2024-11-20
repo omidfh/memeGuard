414e22cb8b1ae0f5f36f0fa0ca420951e6051a8b
omid-faghihi <fhomid3@gmail.com>
2024-11-21 00:23:56 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,27 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-// import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": "/src",
-//       buffer: "buffer",
-//     },
-//   },
-//   define: {
-//     global: "window",
-//   },
-//   optimizeDeps: {
-//     esbuildOptions: {
-//       plugins: [
-//         NodeGlobalsPolyfillPlugin({
-//           buffer: true,
-//         }),
-//         NodeModulesPolyfillPlugin(),
-//       ],
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
+      buffer: "buffer",
+    },
+  },
+  define: {
+    global: "window",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
   },
 });
