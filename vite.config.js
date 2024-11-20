128b676e94bbc445a7c429fb9a21edd13ef3748f
omid-faghihi <fhomid3@gmail.com>
2024-11-21 00:27:08 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,41 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+// import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": "/src",
+//       buffer: "buffer",
+//     },
+//   },
+//   define: {
+//     global: "window",
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       plugins: [
+//         NodeGlobalsPolyfillPlugin({
+//           buffer: true,
+//         }),
+//         NodeModulesPolyfillPlugin(),
+//       ],
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-      buffer: "buffer",
-    },
-  },
-  define: {
-    global: "window",
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
-  },
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
 });
