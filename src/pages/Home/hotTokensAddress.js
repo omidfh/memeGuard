dfc5729f2fbaf8b30956738244e8a797c46d8f94
omid-faghihi <fhomid3@gmail.com>
2024-11-23 17:12:59 +0300

    update

--- src/pages/Home/hotTokensAddress.js
+++ src/pages/Home/hotTokensAddress.js
@@ -1,8 +1,8 @@
 export const tokenAddress = [
-  "EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT",
-  "EQCvxJy4eG8hyHBFsZ7eePxrRsUQSFE_jpptRAYBmcG_DOGS",
-  "EQB4zZusHsbU2vVTPqjhlokIOoiZhEdCMT703CWEzhTOo__X",
-  "EQAJ8uWd7EBqsmpSWaRdf_I-8R8-XHwh3gsNKhy-UrdrPcUo",
-  "EQD-cvR0Nz6XAyRBvbhz-abTrRC6sI5tvHvvpeQraV9UAAD7",
+  "EQAZwJdXCZoO9JIbwBTL2a_zzOAPheLICa4YG7lNIlDZzMmx",
+  "EQBtDmYgCKk3ecQ1x-j0CLZRnPYyaV7cB33ad036QN-HE2C7",
+  "EQAZzvY6tSq2IghR8S3mXgc387Wl6yX54MVQdnFJ2EHQ94CB",
+  "EQBTcytZjdZwFLj6TO7CeGvn4aMmqXvbX-nODiApbd011gT3",
+  "EQCcYUWKhjGxwqtk_je9LW2V4RoFmbhOUR9e404Y6FQTEXHh",
   "EQDjeocqoV44Y3miqstuQ0cT5BxSV05E2qEV2VY1OXRQJGGB",
 ];
