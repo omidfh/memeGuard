ad6281f0873801675719821cf62d48e52b191fb9
omid-faghihi <fhomid3@gmail.com>
2025-03-09 12:57:13 +0330

    Update hotTokensAddress.js

--- src/pages/Home/hotTokensAddress.js
+++ src/pages/Home/hotTokensAddress.js
@@ -1,8 +1,8 @@
 export const tokenAddress = [
   "EQB420yQsZobGcy0VYDfSKHpG2QQlw-j1f_tPu1J488I__PX",
   "EQB-vc00g9PeUWLptSAH4g1J5kYS7WTgtVEfhI6oKdQtRudE",
-  "EQDol2oVpmBznAL6vQpF51QW3p0PKV7ag4VEtex8_MeMHASH",
   "EQCvxJy4eG8hyHBFsZ7eePxrRsUQSFE_jpptRAYBmcG_DOGS",
   "EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT",
+  "EQDol2oVpmBznAL6vQpF51QW3p0PKV7ag4VEtex8_MeMHASH",
   "EQDWXjnVWheFemaAaFn-Cp4nDehvGllrXOZ8wqHm8sDEwn_c",
 ];
